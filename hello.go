c4e69cb53ffcb541ce343e25f051d67430bdf87f
Pham The Manh <manhpham85@gmail.com>
2018-03-10 13:09:12 +0700

    faefawefawefaewfa

--- hello.go
+++ hello.go
@@ -1,32 +1,13 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
-	"log"
-
-	_ "github.com/mattn/go-sqlite3"
+	"time"
 )
 
 func main() {
-	db, err := sql.Open("sqlite3", "./testSqlite.db")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
-
-	rows, err := db.Query("select id, name from tbl1")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer rows.Close()
-	for rows.Next() {
-		var id int
-		var name string
-		err = rows.Scan(&id, &name)
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Println(id, name)
+	for {
+		fmt.Println("x")
+		time.Sleep(time.Second)
 	}
 }
